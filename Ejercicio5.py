9861d6f1a9065212e32cbb2b3d6f563bd0bd6f3c
RaquelG18 <orgarces@espe.edu.ec>
2022-05-17 17:05:00 -0500

    update

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -9,8 +9,9 @@ multiplicacion = numero1*numero2  #Operación multiplicacion
 potenciacion = numero1**numero2  #Operación potenciacion
 
 if numero2 != 0:    #Condicional para la operación de la división, si numero2 es diferente de 0 se realiza la operación. Caso contrario se muestra un mensaje para que el usuario ingrese un valor diferente de 0. 
-    division = numero1/numero2  #Opreación divisoón
-    print("La suma es:", suma, "\n La resta:", resta,
-          "\n La multiplicación es:", multiplicacion, "\n La división es:", division, "\n La potencición es:", potenciacion)#Imprime por consola 
+    division = numero1/numero2  #Opreación divisón
+    divisionEntera = numero1 // numero2 #Opreación divisón entera 
+    print("\n La suma es:", suma, "\n La resta:", resta,
+          "\n La multiplicación es:", multiplicacion, "\n La división es:", division, "\n La división entera es:", divisionEntera ,"\n La potencición es:", potenciacion)#Imprime por consola 
 else:
     print("Ingrese un número entero diferente de 0")#Imprime el mesaje por consola de la condicional 
